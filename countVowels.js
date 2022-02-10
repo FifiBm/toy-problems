129361708fc5b57dd0028e43900a5ac5817bc436
Kayla Golder <golderkayla@gmail.com>
2022-02-10 15:38:13 -0500

    Update countVowels.js

--- countVowels.js
+++ countVowels.js
@@ -1,4 +1,9 @@
-//
+//complete the function below
+//a function that takes in a string and returns the number of vowels in the string
+//ex. countVowels('hey there you') would output 5
+
 const countVowels = (str) => {
   
 }
+
+console.log(countVowels('flowers'); // 2
