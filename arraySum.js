3aee41d32add554521a3cc19b93db9bca935e0c3
Kayla Golder <golderkayla@gmail.com>
2022-02-10 15:31:01 -0500

    Create arraySum.js

---
+++ arraySum.js
@@ -0,0 +1,8 @@
+//complete the function below
+//a function that takes in an array and returns the sum of all the elements in the array
+//ex. arraySum([1,3]) would output 4
+
+const arraySum = (array) => {
+  
+}
+console.log(arraySum([1,2,3,2]);// should output 8
